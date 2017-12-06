a32893d0b688b0fa3c271f4e7976da2bc76fad9b
markeisenberg <markeisenberg43@gmail.com>
2017-12-06 18:07:28 +0000

    v1 commit

---
+++ formoid_files/formoid1/form.php
@@ -0,0 +1,46 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', '');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'message');
+define('FINISH_MESSAGE', 'Thanks for filling out my form!');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+define('_DIR_', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require_once _DIR_ . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-blue.css" type="text/css" />
+<span class="alert alert-success"><?php echo FINISH_MESSAGE; ?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-solid-blue.css" type="text/css" />
+<script type="text/javascript" src="<?php echo dirname($form_path); ?>/jquery.min.js"></script>
+<form class="formoid-solid-blue" style="background-color:#FFFFFF;font-size:14px;font-family:'Roboto',Arial,Helvetica,sans-serif;color:#34495E;max-width:480px;min-width:150px" method="post"><div class="title"><h2>My equatic life</h2></div>
+	<div class="element-separator"><hr><h3 class="section-break-title">Age</h3></div>
+	<div class="element-number<?php frmd_add_class("number"); ?>"><label class="title"></label><div class="item-cont"><input class="large" type="text" min="0" max="100" name="number" placeholder="Number" value=""/><span class="icon-place"></span></div></div>
+	<div class="element-separator"><hr><h3 class="section-break-title">Height</h3></div>
+	<div class="element-separator"><hr><h3 class="section-break-title">Hair colour</h3></div>
+	<div class="element-select<?php frmd_add_class("select"); ?>"><label class="title"></label><div class="item-cont"><div class="large"><span><select name="select" >
+
+		<option value="blonde">blonde</option>
+		<option value="brown">brown</option>
+		<option value="black">black</option>
+		<option value="grey">grey</option>
+		<option value="red">red</option></select><i></i><span class="icon-place"></span></span></div></div></div>
+	<div class="element-separator"><hr><h3 class="section-break-title">Favourite hobby</h3></div>
+	<div class="element-radio<?php frmd_add_class("radio"); ?>"><label class="title"></label>		<div class="column column4"><label><input type="radio" name="radio" value="Skiing" /><span>Skiing</span></label></div><span class="clearfix"></span>
+		<div class="column column4"><label><input type="radio" name="radio" value="Surfing" /><span>Surfing</span></label></div><span class="clearfix"></span>
+		<div class="column column4"><label><input type="radio" name="radio" value="Kayaking" /><span>Kayaking</span></label></div><span class="clearfix"></span>
+		<div class="column column4"><label><input type="radio" name="radio" value="Workout" /><span>Workout</span></label></div><span class="clearfix"></span>
+</div>
+	<div class="element-separator"><hr><h3 class="section-break-title">Face shape</h3></div>
+<div class="submit"><input type="submit" value="Go!"/></div></form><script type="text/javascript" src="<?php echo dirname($form_path); ?>/formoid-solid-blue.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
